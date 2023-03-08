56c4d5d94c652e7f2a63e53fe2f0dbbea79322dc
Jordan Lenard <leonard_2010@live.com>
2023-03-07 21:49:11 -0600

    notes

--- calculator.js
+++ calculator.js
@@ -3,3 +3,10 @@ let x = 0;
 let y = 0;
 const lowerArr = [];
 const upperArr = [];
+const lowerNum = document.getElementById("lower");
+const upperNum = document.getElementById("upper");
+//add event listener to each square?
+//can a fucntion have all operators in if statements so no need for multiple functions?
+function operator() {
+
+}
