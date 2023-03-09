3a30410428982df7b053af28d72d318353ae1389
Jordan Lenard <leonard_2010@live.com>
2023-03-08 21:45:55 -0600

    add event listeners to all numbers

--- calculator.js
+++ calculator.js
@@ -1,12 +1,24 @@
 //functions for each operator +,-,*, and /
-let x = 0;
-let y = 0;
-const lowerArr = [];
-const upperArr = [];
 const lowerNum = document.getElementById("lower");
 const upperNum = document.getElementById("upper");
+const lowerArr = Array.from(lowerNum);
+const upperArr = Array.from(upperNum);
+
 //add event listener to each square?
 //can a fucntion have all operators in if statements so no need for multiple functions?
-function operator() {
+//get document by id add event listener of click to run one function
+//that function will get value of that element and push to lower arr first
+document.getElementById("square9").addEventListener("click", enterNums);
+document.getElementById("square8").addEventListener("click", enterNums);
+document.getElementById("square7").addEventListener("click", enterNums);
+document.getElementById("square6").addEventListener("click", enterNums);
+document.getElementById("square5").addEventListener("click", enterNums);
+document.getElementById("square4").addEventListener("click", enterNums);
+document.getElementById("square3").addEventListener("click", enterNums);
+document.getElementById("square2").addEventListener("click", enterNums);
+document.getElementById("square1").addEventListener("click", enterNums);
+document.getElementById("square0").addEventListener("click", enterNums);
+
+function enterNums() {
 
 }
