f6173e7377ccdd630584de6358229bdaa7c9779d
Jordan Lenard <leonard_2010@live.com>
2023-03-13 21:07:50 -0500

    created function that addes number to array

--- calculator.js
+++ calculator.js
@@ -1,8 +1,9 @@
 //functions for each operator +,-,*, and /
-const lowerNum = document.getElementById("lower");
+
 const upperNum = document.getElementById("upper");
-const lowerArr = Array.from(lowerNum);
-const upperArr = Array.from(upperNum);
+
+const upperArr = [];
+
 
 //add event listener to each square?
 //can a fucntion have all operators in if statements so no need for multiple functions?
@@ -10,8 +11,12 @@ const upperArr = Array.from(upperNum);
 //that function will get value of that element and push to lower arr first
 
 let squares = document.querySelectorAll(".square");
+const lowerArr = [];
+const lowerNum = document.getElementById("lower");
 squares.forEach(function(square) {
     square.addEventListener('click', function() {
-        let squareID = this.getAttribute('id');        
+        let squareValue = this.innerHTML;
+        lowerArr.push(squareValue);
+        lowerNum.innerHTML = lowerArr.join("");
     });
 });
