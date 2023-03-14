c55bb1f129d1c91716c5e85d864d19c65f50c2d4
Jordan Lenard <leonard_2010@live.com>
2023-03-13 20:15:50 -0500

    created function to log getting id of square

--- calculator.js
+++ calculator.js
@@ -8,17 +8,10 @@ const upperArr = Array.from(upperNum);
 //can a fucntion have all operators in if statements so no need for multiple functions?
 //get document by id add event listener of click to run one function
 //that function will get value of that element and push to lower arr first
-document.getElementById("square9").addEventListener("click", enterNums);
-document.getElementById("square8").addEventListener("click", enterNums);
-document.getElementById("square7").addEventListener("click", enterNums);
-document.getElementById("square6").addEventListener("click", enterNums);
-document.getElementById("square5").addEventListener("click", enterNums);
-document.getElementById("square4").addEventListener("click", enterNums);
-document.getElementById("square3").addEventListener("click", enterNums);
-document.getElementById("square2").addEventListener("click", enterNums);
-document.getElementById("square1").addEventListener("click", enterNums);
-document.getElementById("square0").addEventListener("click", enterNums);
 
-function enterNums() {
-
-}
+let squares = document.querySelectorAll(".square");
+squares.forEach(function(square) {
+    square.addEventListener('click', function() {
+        let squareID = this.getAttribute('id');        
+    });
+});
