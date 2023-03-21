c14a172f9eda6193417eddce6bcb25275237ae5d
Jordan Lenard <leonard_2010@live.com>
2023-03-20 21:52:47 -0500

    redo of calc to have numbers and ops move in one

--- calculator.js
+++ calculator.js
@@ -12,62 +12,58 @@ backspaceButton.addEventListener("click", function() {
 });
 
 
-//add event listener to each square?
-//can a fucntion have all operators in if statements so no need for multiple functions?
-//get document by id add event listener of click to run one function
-//that function will get value of that element and push to lower arr first
+//upper will be blank, but when an operator is clicked the lower array will be added to the upper as an array (array inside an array)
+// 
 
-let squares = document.querySelectorAll(".square");
-const lowerArr = [];
 const upperArr = [];
-const lowerNum = document.getElementById("lower");
-const upperNum = document.getElementById("upper");
-let operators = ["+", "-", "*", "/"];
-let equal = ["="];
-squares.forEach(function(square) {
-    square.addEventListener('click', function() {
-        if(!isNaN(this.innerHTML) || this.innerHTML === "."){
-            let squareValue = this.innerHTML;
-            lowerArr.push(squareValue);
-            lowerNum.innerHTML = lowerArr.join("");
-        }else if (operators.includes(this.innerHTML)) {
-            lowerArr.push(this.innerHTML);
-            upperArr.push(lowerArr.join(""));
-            upperNum.innerHTML = upperArr.join("");
-            lowerArr.length = 0;
-        }else if (this.innerHTML == "=") {
-            console.log(performCalculation());
-        }
+const lowerArr = [];
+const squares = document.querySelectorAll(".square");
+const lowerNum = document.querySelector("#lower");
+const upperNum = document.querySelector("#upper");
 
-    });
-});
 
-//function that gets the operator and does that operation and returns the sum
-function performCalculation() {
-    //have return statement after each if statment
-    let operator = upperArr.pop();
-    let firstNum = upperArr.slice(0, -1);
-    let num1 = Number(firstNum.join(""));
-    let num2 = Number(lowerArr.join(""));
-
-    if (operator == "+") {
-        let sum = num1 + num2;
-    } else if (operator == "-") {
-        let sum = num1 - num2;
-    } else if (operator == "*") {
-        let sum = num1 * num2;
-    } else if (operator == "/" && num2 == 0) {
-        alert("You can not divide by zero.");
-        return "Error";
-    } else if (operator == "/") {
-        let sum = num1 / num2;
+squares.forEach(square => {
+    if(square.classList.contains("number")) {
+        square.addEventListener("click", () => {
+            const value = square.textContent;
+            lowerArr.push(value);
+            lowerNum.textContent = lowerArr.join("");
+        })
+    }else if (square.classList.contains("operator")) {
+        square.addEventListener("click", () => {
+            const value = square.textContent;
+            if (value !== "=" && upperArr.length == 0) {
+                lowerArr.push(value);
+                upperArr.push(lowerArr.join(""));
+                upperNum.textContent = upperArr.join("");
+                lowerArr.length = 0;
+                lowerNum.textContent = lowerArr.join("");
+            }else if (value !== "=" && upperArr.length !== 0) {
+                //need to convert upperArr to array so to seperate 2 and operator and pop off to get operator
+                const num1 = parseFloat(upperArr.join(""));
+                const num2 = parseFloat(lowerArr.join(""));
+                switch (getOp) {
+                    case "+":
+                        lowerArr = [num1 + num2];
+                        break;
+                    case "-":
+                        lowerArr = [num1 - num2];
+                        break;
+                    case "*":
+                        lowerArr = [num1 * num2];
+                        break;
+                    case "/":
+                        if (num2 !== 0) {
+                            lowerArr = num1 / num2;
+                        }else {
+                            alert("You can not divide by zero");
+                        }
+                        break;
+                    default:
+                        alert("Error: Invalid operator")
+                }
+                lowerNum.textContent = lowerArr.join("");
+            }
+        })
     }
-    return sum;
-    console.log(sum);
-    upperArr.push(operator, num2);
-    upperNum.innerHTML = upperArr.join("");
-    lowerArr.length = 0;
-    lowerArr.push(sum);
-    lowerNum.innerHTML = lowerArr.join("");
-
-}
+})
