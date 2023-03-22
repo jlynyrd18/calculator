797893d9836f25cebefb31c2aa1c2f483f899066
Jordan Lenard <leonard_2010@live.com>
2023-03-21 21:52:56 -0500

    removed brackets to see if NaN persists wrong

--- calculator.js
+++ calculator.js
@@ -47,13 +47,13 @@ squares.forEach(square => {
                 let result = 0;
                 switch (operator) {
                     case "+":
-                        result = [num1 + num2];
+                        result = num1 + num2;
                         break;
                     case "-":
-                        result = [num1 - num2];
+                        result = num1 - num2;
                         break;
                     case "*":
-                        result = [num1 * num2];
+                        result = num1 * num2;
                         break;
                     case "/":
                         if (num2 !== 0) {
