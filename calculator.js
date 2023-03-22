066fb034e8b70de9fb55fb6839b1e1c42ecdeadf
Jordan Lenard <leonard_2010@live.com>
2023-03-21 21:50:25 -0500

    framework but not finalizing number

--- calculator.js
+++ calculator.js
@@ -13,7 +13,7 @@ backspaceButton.addEventListener("click", function() {
 
 
 //upper will be blank, but when an operator is clicked the lower array will be added to the upper as an array (array inside an array)
-// 
+
 
 const upperArr = [];
 const lowerArr = [];
@@ -40,21 +40,24 @@ squares.forEach(square => {
                 lowerNum.textContent = lowerArr.join(" ");
             }else if (value !== "=" && upperArr.length !== 0) {
                 //need to convert upperArr to array so to seperate 2 and operator and pop off to get operator
+                const operator = upperArr[0].match(/[\+\-\*\/]/);
+                console.log(operator);
                 const num1 = parseFloat(upperArr.join(""));
                 const num2 = parseFloat(lowerArr.join(""));
-                switch (getOp) {
+                let result = 0;
+                switch (operator) {
                     case "+":
-                        lowerArr = [num1 + num2];
+                        result = [num1 + num2];
                         break;
                     case "-":
-                        lowerArr = [num1 - num2];
+                        result = [num1 - num2];
                         break;
                     case "*":
-                        lowerArr = [num1 * num2];
+                        result = [num1 * num2];
                         break;
                     case "/":
                         if (num2 !== 0) {
-                            lowerArr = num1 / num2;
+                            result = num1 / num2;
                         }else {
                             alert("You can not divide by zero");
                         }
@@ -62,7 +65,48 @@ squares.forEach(square => {
                     default:
                         alert("Error: Invalid operator")
                 }
-                lowerNum.textContent = lowerArr.join("");
+                //updated output of what is resolved
+                upperArr[0] = result; // update the upperArr with the new result
+                upperArr.push(value);
+                upperNum.textContent = upperArr.join(""); // update the upperNum display
+                lowerArr.length = 0; // reset the lowerArr
+                lowerNum.textContent = ""; // reset the lowerNum display
+
+            }else if (value === "=" && upperArr.length !== 0 && lowerArr.length !== 0) {
+            const operator = upperArr[0].match(/[+\-*/]/);
+            const num1 = parseFloat(upperArr.slice(1).join(""));
+            const num2 = parseFloat(lowerArr.join(""));
+            let result = 0;
+            switch (operator[0]) {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "/":
+                    if (num2 !== 0) {
+                        result = num1 / num2;
+                    } else {
+                        upperArr.length = 0;
+                        upperNum.textContent = "Invalid operator";
+                        return;
+                    }
+                    break;
+                default:
+                    upperArr.length = 0;
+                    upperNum.textContent = "Invalid operator";
+                    return;
+            }
+            upperArr.length = 0;
+            upperNum.textContent = "";
+            lowerArr.length = 0;
+            lowerArr.push(result.toString());
+            lowerNum.textContent = result.toString();
+
             }
         })
     }
