ab56a72d522d5cee17c0d1a0c2a0e87f2223c873
Jordan Lenard <leonard_2010@live.com>
2023-03-22 21:34:55 -0500

    completed simple 2+3 math

--- calculator.js
+++ calculator.js
@@ -14,7 +14,7 @@ backspaceButton.addEventListener("click", function() {
 
 //upper will be blank, but when an operator is clicked the lower array will be added to the upper as an array (array inside an array)
 
-
+let x = 0;
 const upperArr = [];
 const lowerArr = [];
 const squares = document.querySelectorAll(".square");
@@ -22,92 +22,43 @@ const lowerNum = document.querySelector("#lower");
 const upperNum = document.querySelector("#upper");
 
 
-squares.forEach(square => {
-    if(square.classList.contains("number")) {
-        square.addEventListener("click", () => {
-            const value = square.textContent;
+squares.forEach(function(square) {
+    square.addEventListener("click", function(){
+        const value = square.textContent;
+        //add if statment if upper and lower arrays are not 0 to clear them out?
+        if (value !== "=" && square.classList.contains("number")) {
             lowerArr.push(value);
-            lowerNum.textContent = lowerArr.join("");
-        })
-    }else if (square.classList.contains("operator")) {
-        square.addEventListener("click", () => {
-            const value = square.textContent;
-            if (value !== "=" && upperArr.length == 0) {
-                lowerArr.push(value);
-                upperArr.push(lowerArr.join(" "));
-                upperNum.textContent = upperArr.join(" ");
-                lowerArr.length = 0;
-                lowerNum.textContent = lowerArr.join(" ");
-            }else if (value !== "=" && upperArr.length !== 0) {
-                //need to convert upperArr to array so to seperate 2 and operator and pop off to get operator
-                const operator = upperArr[0].match(/[\+\-\*\/]/);
-                console.log(operator);
-                const num1 = parseFloat(upperArr.join(""));
-                const num2 = parseFloat(lowerArr.join(""));
-                let result = 0;
-                switch (operator) {
-                    case "+":
-                        result = num1 + num2;
-                        break;
-                    case "-":
-                        result = num1 - num2;
-                        break;
-                    case "*":
-                        result = num1 * num2;
-                        break;
-                    case "/":
-                        if (num2 !== 0) {
-                            result = num1 / num2;
-                        }else {
-                            alert("You can not divide by zero");
-                        }
-                        break;
-                    default:
-                        alert("Error: Invalid operator")
-                }
-                //updated output of what is resolved
-                upperArr[0] = result; // update the upperArr with the new result
-                upperArr.push(value);
-                upperNum.textContent = upperArr.join(""); // update the upperNum display
-                lowerArr.length = 0; // reset the lowerArr
-                lowerNum.textContent = ""; // reset the lowerNum display
-
-            }else if (value === "=" && upperArr.length !== 0 && lowerArr.length !== 0) {
-            const operator = upperArr[0].match(/[+\-*/]/);
-            const num1 = parseFloat(upperArr.slice(1).join(""));
-            const num2 = parseFloat(lowerArr.join(""));
-            let result = 0;
-            switch (operator[0]) {
-                case "+":
-                    result = num1 + num2;
+            lowerNum.textContent = lowerArr.join(" ");
+        }else if (value !== "=" && square.classList.contains("operator")) {
+            lowerArr.push(value);
+            upperArr.push(lowerArr.join(" "));
+            upperNum.textContent = upperArr.join(" ");
+            lowerArr.length = 0;  
+        }else if (value == "=") {
+            let operator = upperArr.toString().slice(-1);
+            let num1 = Number.parseFloat(upperArr);
+            let num2 = Number.parseFloat(lowerArr);
+            switch(operator) {
+                case "+": 
+                    x = num1 + num2;
                     break;
-                case "-":
-                    result = num1 - num2;
+                case "-": 
+                    x = num1 - num2;
                     break;
-                case "*":
-                    result = num1 * num2;
+                case "*": 
+                    x = num1 * num2;
                     break;
-                case "/":
-                    if (num2 !== 0) {
-                        result = num1 / num2;
-                    } else {
-                        upperArr.length = 0;
-                        upperNum.textContent = "Invalid operator";
-                        return;
-                    }
+                case "/": 
+                    x = num1 / num2;
                     break;
                 default:
-                    upperArr.length = 0;
-                    upperNum.textContent = "Invalid operator";
-                    return;
+                    alert("No operator selected");
             }
-            upperArr.length = 0;
-            upperNum.textContent = "";
+            upperArr.push(lowerArr.join(""));
+            upperNum.textContent = upperArr.join("");
             lowerArr.length = 0;
-            lowerArr.push(result.toString());
-            lowerNum.textContent = result.toString();
-
-            }
-        })
-    }
+            lowerArr.push(x);
+            lowerNum.textContent = lowerArr;
+        }
+    })
 })
